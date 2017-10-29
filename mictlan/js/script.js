9f19d56e065ecc21a24319131797413771866e15
YisusLOU <sinisterg@hotmail.com>
2017-10-28 23:14:03 -0500

    primeros pasos pagina web mictlan
    se coloco navegador
    logotipo provicional
    y galeria en carousel

---
+++ mictlan/js/script.js
@@ -0,0 +1,14 @@
+;
+(function(d,w,n,c,$){
+    $(d).ready(function(){
+        $(".button-collapse").sideNav()
+    });
+    
+     $(d).ready(function(){
+      $('.carousel').carousel();
+    });
+})
+(Document,window,navigator,console.log,jQuery);
+
+    
+ 
