675b94d5d2ca8faa1cdd03a843309bfa7c6a8e34
YisusLOU <sinisterg@hotmail.com>
2017-11-17 10:56:01 -0600

    clase 7 actividades

--- ejercicios/js/2_dom.js
+++ ejercicios/js/2_dom.js
@@ -1,89 +1,154 @@
-(function(d,c){
-    //manejando selectores con js
-    /*
-    c(
+(function (d, c) {
+  /* ********** IMPORTACIÓN DE MÓDULOS ********** */
+
+  /* ********** DECLARACIÓN DE VARIABLES ********** */
+   const parrafoQueEsDOM = d.getElementById('que-es'),
+    hojaCSS = d.createElement('link'),
+    imagenes = d.getElementById('imagenes'),
+    figureFood = d.createElement('figure'),
+    linkDoc = d.getElementById('link-doc'),
+    header = d.getElementById('header'),
+    articleTeoria = d.getElementById('teoria'),
+    figuras = d.querySelectorAll('.figure'),
+    pEvento = d.getElementById('evento'),
+    btnSaludar = d.getElementById('btn-saludar'),
+    btnCabeceraFija = d.getElementById('btn-cabecera-fija'),
+    btnCabeceraAmarilla = d.getElementById('btn-cabecera-amarilla'),
+    btnCabeceraVerde = d.getElementById('btn-cabecera-verde'),
+    btnToggleFigure = d.getElementById('btn-toggle-figure')
+
+  /* ********** DECLARACIÓN DE FUNCIONES ********** */
+  function saludar () {
+    alert('Hola mundo')
+  }
+
+
+  /* ********** EJECUCIÓN DE CÓDIGO ********** */
+  //Manejo de selectores CSS con JS
+  c(
     d.querySelector('li'),
     d.querySelector('#header'),
     d.querySelector('.figure'),
     d.querySelector('#metodos-dom code'),
     d.querySelectorAll('li'),
     d.querySelectorAll('li').length,
-    d.querySelectorAll('li')[4],   
+    d.querySelectorAll('li')[4],
     d.querySelectorAll('.figure'),
     d.querySelectorAll('#header'),
     d.getElementById('header'),
-    d.querySelectorAll('#metodos-dom code'),
-    )
-    */
-    
-    //declaracion de variables
-    const parrafoQueEsDOM=d.getElementById('que-es')
-        hojaCSS = d.createElement('link')
-        imagenes = d.getElementById('imagenes')
-        figureFood = d.createElement('figure')
-        linkDoc = d.getElementById('link-doc')
-    //ejecutar instrucciones 
-    parrafoQueEsDOM.innerHTML='El <b>modelo de objetod del documento</b>(<i>DOM - Documento objets model </i>) es un API para documentos HTML y XML. Éste Proveé una representación estructural del documento, permitiendo modificar su contenido y representación visual mediante código JS.'
-    
-    hojaCSS.rel = 'stylesheet'
-    hojaCSS.href = './css/2_dom.css'
-    d.head.appendChild(hojaCSS)
-    
-    //manejo de objetos del DOM
-    
-    c(
-    d,//objeto document de JS 
-    d.documentElement,//elemento html
-    d.head,
-    d.body,
-    d.head.querySelector('title'),
-    d.documentElement.lang,//accediendo al valor del atributo lang del elemento HTML
-    
+    d.querySelectorAll('#metodos-dom code')
+  )
+
+  parrafoQueEsDOM.innerText = 'El <b>Modelo de Objetos del Documento</b> (<i>DOM - Document Object Model</i>) es un API para documentos HTML y XML. Éste proveé una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.'
+
+  parrafoQueEsDOM.innerHTML = 'El <b>Modelo de Objetos del Documento</b> (<i>DOM - Document Object Model</i>) es un API para documentos HTML y XML. Éste proveé una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.'
+
+  hojaCSS.rel = 'stylesheet'
+  hojaCSS.href = './css/2_dom.css'
+  d.head.appendChild(hojaCSS)
+
+  //Manejo de objetos del DOM
+  c(
+   d, // objeto document de JS
+   d.documentElement, // Elemento <html>
+   d.head, // Elemento <head>
+   d.body, // elemento <body>
+   d.head.querySelector('title'), // título del documento
+   d.documentElement.lang // accediendo al valor del atributo lang del elemento <html>
+  )
+
+  //Manejo de los elementos del DOM por su posición
+  c(
     imagenes,
-    imagenes.parentElement,// padre del elemento
-    imagenes.children,//hijosdel elemnto
-    imagenes.firstElementChild,//primer hijo del elemento
-    imagenes.lastElementChild,//ultimo hijo del elemento
-    imagenes.previousElementSibling,//elemento hermano previo
-    imagenes.nextElementSibling,//elemento hermano siguiente
-    imagenes.removeChild(imagenes.firstElementChild),//eliminando primer hijo del elemento 
-        
-    figureFood.classList.add('figure'),
-    figureFood.innerHTML = `
-    <img src="http://lorempixel.com/400/400/food" alt="food"> 
-    <figcaption>food</figcaption></figure>`,
-    imagenes.insertBefore(figureFood,
-    imagenes.lastElementChild),
-        
-    )
-    //estableciendo y obteniendo valores de los atributos de la etiqueta HTML
-    c(
+    imagenes.parentElement, // Padre del elemento
+    imagenes.children, // Hijos del elemento
+    imagenes.firstElementChild, // Primer hijo del elemento
+    imagenes.lastElementChild, // Último hijo del elemento
+    imagenes.previousElementSibling, // Elemento hermano previo
+    imagenes.nextElementSibling // Elemento hermano siguiente
+  )
+
+  //Eliminando el primer hijo del elemento
+  imagenes.removeChild(imagenes.firstElementChild)
+
+  //Manejo  de clases CSS en JS
+  c(
+    d.getElementById('header').className, // Devuelve el valor del atributo class en un String
+    d.getElementById('header').classList // Devuelve el valor del atributo class en un Arreglo
+  )
+
+  figureFood.classList.add('figure')
+  figureFood.innerHTML = `
+    <img src= "http://lorempixel.com/400/400/food" alt= "Food">
+    <figcaption>Food</figcaption>
+  `
+  //Inserta un nodo antes del que se indique como referencia
+  imagenes.insertBefore(figureFood, imagenes.lastElementChild)
+
+  //Estableciendo y Obteniendo los valores de los atributos de las etiquetas HTML
+  //Métodos obtenedores (getters)
+  c(
     linkDoc.href,
     linkDoc.getAttribute('href'),
-        d.documentElement.lang,
-        d.documentElement.getAttribute('lang'),
-    )
-    
-    //metodo establece o setters '
-    
-    d.documentElement.lang = 'es-CL',
-    d.documentElement.setAttribute('lang', 'es-MX'),
-        linkDoc.href = 'https://developer.mozilla.org/es/docs/DOM'
-        linkDoc.setAttribute('target','blank'),
-        linkDoc.style.padding('1rem'),
-        linkDoc.style.display('block'),
-        linkDoc.style.width('50px'),
-        linkDoc.style.textAlign('center'),
-        linkDoc.style.marginLeft('auto'),
-        linkDoc.style.marginRight('auto'),
-        linkDoc.style.borderRadius('25rem'),
-        
-        
+    d.documentElement.lang,
+    d.documentElement.getAttribute('lang'),
+    linkDoc.style
+  )
+  //Métodos establecedores (setters)
+  d.documentElement.lang = 'es-CL'
+  d.documentElement.setAttribute('lang', 'es-MX')
+  linkDoc.href = 'https://developer.mozilla.org/es/docs/DOM'
+  linkDoc.setAttribute('target', '_blank')
+  linkDoc.style.padding = '1rem'
+  linkDoc.style.display = 'block'
+  linkDoc.style.width = '50%'
+  linkDoc.style.textAlign = 'center'
+  linkDoc.style.marginLeft = 'auto'
+  linkDoc.style.marginRight = 'auto'
+  linkDoc.style.borderRadius = '.25rem'
+
+  //Eventos
+  pEvento.innerHTML = 'Los eventos son los mecanismos que tenemos en JavaScript para controlar las acciones del usuario y definir el comportamiento del documento en cierto momento o cuando se cumplan ciertas condiciones. Las funciones que se ejecutan en un evento se llaman <b><i>Event Handler</i></b> (Manejadores de Eventos).'
+
+  //Asignar un manejador de Eventos
+  btnSaludar.addEventListener('click', saludar)
+
+  btnCabeceraFija.addEventListener('click', function () {
     c(
-    d.getElementById('header').className,//devuelve el vaor del atributo class en un string
-    
-    d.getElementById('header').classList,//devuelve el vaor del atributo class en un arreglo
+      header.className,
+      header.classList,
+      header.classList.contains('fixed'),
+      header.classList.contains('fix')
     )
-    
-    
-})(document,console.log);
+
+    if ( header.classList.contains('fixed') ) {
+      header.classList.remove('fixed')
+    } else {
+      header.classList.add('fixed')
+    }
+  })
+
+  btnCabeceraAmarilla.addEventListener('click', function () {
+    header.classList.add('bg-yellow')
+    header.classList.remove('bg-green')
+  })
+
+  btnCabeceraVerde.addEventListener('click', function () {
+    header.classList.add('bg-green')
+    header.classList.remove('bg-yellow')
+  })
+
+  btnToggleFigure.addEventListener('click', function () {
+    c(
+      figuras,
+      figuras[2]
+    )
+    //figuras[2].classList.toggle('figure')
+
+    figuras.forEach(function (el) {
+      el.classList.toggle('figure')
+    })
+
+    figureFood.classList.toggle('figure')
+  })
+})(document, console.log);
