246863fdca3b583822d238571f4f375718b438eb
Yisus LOU <32680854+YisusLOU@users.noreply.github.com>
2018-01-08 14:54:56 -0600

    casi final

--- mictlan/js/script.js
+++ mictlan/js/script.js
@@ -7,7 +7,18 @@
      $(d).ready(function(){
       $('.carousel.carousel-slider').carousel({fullWidth: true});
     });
+    
+   $(document).ready(function() {
+    $('select').material_select();
+  });
+    
+    $(document).ready(function(){
+      $('.slider').slider();
+    });
      
+    $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
 })
 (Document,window,navigator,console.log,jQuery);
 
